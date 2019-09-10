20f013a1195b8a3dd4b6d8cdcbfa0b022d96abe9
José Antonio Cegarra <josemster19@gmail.com>
2019-09-10 16:47:18 +0200

    first commit

---
+++ CodigoSemaforos.c
@@ -0,0 +1,246 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/syscall.h>
+#include <semaphore.h>
+
+
+#define TAM_BUFFER 5
+#define NUM_THREADS 8
+#define NUM_TREBALLS 100000
+typedef struct{
+	int rango_desde;
+	int rango_hasta;
+} trabajo_t;
+
+typedef struct {
+	int pos_lectura;
+	int pos_escritura;
+	int num_trabajos;
+	trabajo_t trabajo[TAM_BUFFER]; 
+} buffer_trabajo_t;
+
+void buffer_trabajos_inicializar(buffer_trabajo_t* buffer_trabajos){
+
+	buffer_trabajos->pos_lectura=0;
+	buffer_trabajos->pos_escritura=0;
+	buffer_trabajos->num_trabajos=0;
+}
+
+pthread_t thread[NUM_THREADS];
+buffer_trabajo_t buffer;
+sem_t mutex;
+sem_t mutex2;
+
+int buffer_trabajos_lleno(buffer_trabajo_t* buffer_trabajos){
+
+int retorno;
+
+	if(buffer_trabajos->num_trabajos == TAM_BUFFER){
+
+	retorno = 1;
+
+	}else{
+
+	retorno=0;
+
+	}
+return retorno;
+}
+
+
+int buffer_trabajos_vacio(buffer_trabajo_t* buffer_trabajos){
+
+int retorno;
+
+	if(buffer_trabajos->num_trabajos == 0){
+
+	retorno = 1;
+
+	}else{
+
+	retorno=0;
+
+	}
+return retorno;
+}
+
+
+void buffer_trabajos_encolar(buffer_trabajo_t* buffer_trabajos, trabajo_t* trabajo){
+
+	
+	buffer_trabajos->trabajo[buffer_trabajos->pos_escritura] = *trabajo;
+	buffer_trabajos->num_trabajos++;
+	buffer_trabajos->pos_escritura = (buffer_trabajos->pos_escritura + 1) % TAM_BUFFER;
+	
+
+}
+
+void buffer_trabajos_atender(buffer_trabajo_t* buffer_trabajos, trabajo_t* trabajo){
+
+	
+	*trabajo = buffer_trabajos->trabajo[buffer_trabajos->pos_lectura];
+	buffer_trabajos->num_trabajos--;
+	buffer_trabajos->pos_lectura = (buffer_trabajos->pos_lectura + 1) % TAM_BUFFER;
+
+
+
+}
+
+int calcularprimo(int numero)
+{
+	int retorno=0;
+	int a=0;
+	for(int i=1;i<=numero;i++)
+	{
+   		if(numero%i==0) 
+		{
+   		 	a++;
+		}
+	}
+	if (a==2)
+	{
+	retorno=1;
+	}
+	return retorno;
+}
+
+
+void primo(trabajo_t* trabajo, int threadid){
+
+int i,a;
+a=0; 
+int numero=trabajo->rango_desde;
+
+
+printf("[Thread %d] Primos en [%d,%d]:",threadid, trabajo->rango_desde,trabajo->rango_hasta);
+while(numero<=trabajo->rango_hasta){
+	
+	if (calcularprimo(numero) == 1)
+	{
+		printf("%d ", numero);
+	}
+	numero=numero+1;
+	
+}
+printf("\n");
+
+}
+
+
+
+
+
+void enviar_trabajo(buffer_trabajo_t* buffer, trabajo_t* trabajo){
+
+	sem_wait(&mutex);
+	while(buffer_trabajos_lleno(buffer)==1)
+	{
+		sem_post(&mutex);
+		sem_wait(&mutex);
+
+	}
+	buffer_trabajos_encolar(buffer,trabajo);
+
+	sem_post(&mutex);
+}
+
+
+
+
+void recibir_trabajo(trabajo_t* trabajo){
+
+	
+	sem_wait(&mutex);
+
+	while(buffer_trabajos_vacio(&buffer)==1)
+	{
+		sem_post(&mutex);
+		sem_wait(&mutex);
+
+	}
+	buffer_trabajos_atender(&buffer, trabajo);
+	sem_post(&mutex);
+	
+
+
+}
+
+void* FuncionSlave(int threadid){
+	trabajo_t trabajo;
+	int seguir =1;
+
+	
+	while(seguir == 1){
+
+		recibir_trabajo(&trabajo);
+		sem_wait(&mutex2);
+		if(trabajo.rango_desde == -1){
+		sem_post(&mutex2);
+		break;}
+		else{
+		primo(&trabajo,threadid);
+		}
+		sem_post(&mutex2);
+
+		
+	}
+	
+	pthread_exit(NULL);
+}
+
+
+//Crear 5 trabajos extra
+void CrearTrabajos(buffer_trabajo_t* buffer){
+	int z;
+	
+	
+	for(z=1;z<NUM_TREBALLS;z+=10){
+		trabajo_t trabajo;
+		trabajo.rango_desde=z;
+		trabajo.rango_hasta=z+9;
+		enviar_trabajo(buffer,&trabajo);
+		}
+	for(z=0;z<=NUM_THREADS;z++){
+		trabajo_t trabajo;
+		trabajo.rango_desde=-1;
+		trabajo.rango_hasta=-1;
+		enviar_trabajo(buffer,&trabajo);
+		}
+	}
+	
+	
+	
+
+
+
+
+
+int main(int argc,char** argv) {
+// Crear trabajos
+	
+	
+	
+	
+	buffer_trabajos_inicializar(&buffer);
+	sem_init(&mutex,0,8);
+	sem_init(&mutex2,0,1);
+	
+	long int i;
+	for (i=0;i<NUM_THREADS;++i) {
+
+		pthread_create(thread+i,NULL,(void* (*)(void*))FuncionSlave,(void*)(i));
+	}
+	CrearTrabajos(&buffer);
+	
+// Esperar threads
+	for (i=0;i<NUM_THREADS;i++) {
+		pthread_join(thread[i],NULL);
+		printf("\n Eliminando thread %ld",i);
+
+	}
+return 0;
+}
